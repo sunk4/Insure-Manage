8a515a1e55454ebd3a4ae75f2294354626fe4e4a
Roman Trnka <trnka.roman13@gmail.com>
2024-12-29 11:53:30 +0100

    custom errors added

---
+++ backend/src/main/java/com/roman/insure_manage/exception/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.roman.insure_manage.exception;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
+@Getter
+@Setter
+@Builder
+public class ErrorResponse {
+    private final String message;
+    private final int code;
+    private final LocalDateTime timestamp;
+
+}
